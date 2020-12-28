30e29d85a28c28b84ab1ee10a97008c6f8154d09
Filip-tr-08 <filiptrajo@yahoo.com>
2020-12-28 16:44:01 +0100

    Update script.js

--- js domasna 2/script/script.js
+++ js domasna 2/script/script.js
@@ -5,14 +5,14 @@ var n3 = parseInt(prompt("Vnesi go tretiot broj"));
 var n4 = parseInt(prompt("Vnesi go cetvrtiot broj"));
 var n5 = parseInt(prompt("Vnesi go petiot broj"));
 
-if (n1 > n2 && n1 > n3 && n1 > n4 && n1 > n5) {
+if (n1 >= n2 && n1 >= n3 && n1 >= n4 && n1 >= n5) {
   alert("Prviot broj: " + n1 + " e najgolem!");
-} else if (n2 > n1 && n2 > n3 && n2 > n4 && n2 > n5) {
+} else if (n2 >= n1 && n2 >= n3 && n2 >= n4 && n2 >= n5) {
   alert("Vtoriot broj: " + n2 + " e najgolem!");
-} else if (n3 > n1 && n3 > n2 && n3 > n4 && n3 > n5) {
+} else if (n3 >= n1 && n3 >= n2 && n3 >= n4 && n3 >= n5) {
   alert("Tretiot broj: " + n3 + " e najgolem!");
-} else if (n4 > n1 && n4 > n2 && n4 > n3 && n4 > n5) {
+} else if (n4 >= n1 && n4 >= n2 && n4 >= n3 && n4 >= n5) {
   alert("Cetvriot broj: " + n4 + " e najgolem!");
-} else if (n5 > n1 && n5 > n2 && n5 > n3 && n5 > n4) {
+} else if (n5 >= n1 && n5 >= n2 && n5 >= n3 && n5 >= n4) {
   alert("Petiot broj: " + n5 + " e najgolem!");
 }
